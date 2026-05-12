eb716f5482634517f8854faebc30c35c01e7f0c3
putraerlang2-glitch <putraerlang2@gmail.com>
2026-05-12 08:13:14 +0700

    penambahan fitur hapus keranjang dan implementasi table drivendi button hapus

--- FormTransaksi.Designer.cs
+++ FormTransaksi.Designer.cs
@@ -44,55 +44,54 @@ namespace TubesKPL
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.BtnHapus = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.TabelKeranjang)).BeginInit();
             this.SuspendLayout();
             // 
             // BoxObat
             // 
             this.BoxObat.FormattingEnabled = true;
-            this.BoxObat.Location = new System.Drawing.Point(92, 33);
-            this.BoxObat.Margin = new System.Windows.Forms.Padding(2);
+            this.BoxObat.Location = new System.Drawing.Point(123, 41);
+            this.BoxObat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BoxObat.Name = "BoxObat";
-            this.BoxObat.Size = new System.Drawing.Size(117, 21);
+            this.BoxObat.Size = new System.Drawing.Size(155, 24);
             this.BoxObat.TabIndex = 0;
             this.BoxObat.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 36);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(53, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(63, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cari Obat";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 64);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(35, 79);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(82, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Jumlah Obat";
             // 
             // TBjumlah
             // 
-            this.TBjumlah.Location = new System.Drawing.Point(92, 64);
-            this.TBjumlah.Margin = new System.Windows.Forms.Padding(2);
+            this.TBjumlah.Location = new System.Drawing.Point(123, 79);
+            this.TBjumlah.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TBjumlah.Multiline = true;
             this.TBjumlah.Name = "TBjumlah";
-            this.TBjumlah.Size = new System.Drawing.Size(117, 19);
+            this.TBjumlah.Size = new System.Drawing.Size(155, 22);
             this.TBjumlah.TabIndex = 3;
             this.TBjumlah.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // BtnHitung
             // 
-            this.BtnHitung.Location = new System.Drawing.Point(138, 171);
-            this.BtnHitung.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnHitung.Location = new System.Drawing.Point(184, 210);
+            this.BtnHitung.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnHitung.Name = "BtnHitung";
-            this.BtnHitung.Size = new System.Drawing.Size(70, 21);
+            this.BtnHitung.Size = new System.Drawing.Size(93, 26);
             this.BtnHitung.TabIndex = 4;
             this.BtnHitung.Text = "Checkout";
             this.BtnHitung.UseVisualStyleBackColor = true;
@@ -100,10 +99,10 @@ namespace TubesKPL
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 137);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(133, 169);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 21);
+            this.button1.Size = new System.Drawing.Size(145, 26);
             this.button1.TabIndex = 5;
             this.button1.Text = "Tambah Pesanan";
             this.button1.UseVisualStyleBackColor = true;
@@ -112,22 +111,21 @@ namespace TubesKPL
             // TabelKeranjang
             // 
             this.TabelKeranjang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.TabelKeranjang.Location = new System.Drawing.Point(266, 36);
-            this.TabelKeranjang.Margin = new System.Windows.Forms.Padding(2);
+            this.TabelKeranjang.Location = new System.Drawing.Point(309, 43);
+            this.TabelKeranjang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TabelKeranjang.Name = "TabelKeranjang";
             this.TabelKeranjang.RowHeadersWidth = 51;
             this.TabelKeranjang.RowTemplate.Height = 24;
-            this.TabelKeranjang.Size = new System.Drawing.Size(226, 122);
+            this.TabelKeranjang.Size = new System.Drawing.Size(479, 193);
             this.TabelKeranjang.TabIndex = 6;
             this.TabelKeranjang.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(264, 20);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(315, 25);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.Size = new System.Drawing.Size(68, 16);
             this.label3.TabIndex = 7;
             this.label3.Text = "Keranjang";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -135,26 +133,27 @@ namespace TubesKPL
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(264, 180);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(310, 254);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.Size = new System.Drawing.Size(72, 16);
             this.label4.TabIndex = 8;
             this.label4.Text = "Total: Rp.0";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // TBUangBayar
             // 
-            this.TBUangBayar.Location = new System.Drawing.Point(92, 101);
+            this.TBUangBayar.Location = new System.Drawing.Point(123, 124);
+            this.TBUangBayar.Margin = new System.Windows.Forms.Padding(4);
             this.TBUangBayar.Name = "TBUangBayar";
-            this.TBUangBayar.Size = new System.Drawing.Size(117, 20);
+            this.TBUangBayar.Size = new System.Drawing.Size(155, 22);
             this.TBUangBayar.TabIndex = 9;
             // 
             // BtnUbahConfig
             // 
-            this.BtnUbahConfig.Location = new System.Drawing.Point(417, 175);
+            this.BtnUbahConfig.Location = new System.Drawing.Point(514, 247);
+            this.BtnUbahConfig.Margin = new System.Windows.Forms.Padding(4);
             this.BtnUbahConfig.Name = "BtnUbahConfig";
-            this.BtnUbahConfig.Size = new System.Drawing.Size(75, 23);
+            this.BtnUbahConfig.Size = new System.Drawing.Size(100, 28);
             this.BtnUbahConfig.TabIndex = 10;
             this.BtnUbahConfig.Text = "Ubah Config";
             this.BtnUbahConfig.UseVisualStyleBackColor = true;
@@ -162,50 +161,67 @@ namespace TubesKPL
             // 
             // TBPajak
             // 
-            this.TBPajak.Location = new System.Drawing.Point(392, 204);
+            this.TBPajak.Location = new System.Drawing.Point(481, 283);
+            this.TBPajak.Margin = new System.Windows.Forms.Padding(4);
             this.TBPajak.Name = "TBPajak";
-            this.TBPajak.Size = new System.Drawing.Size(100, 20);
+            this.TBPajak.Size = new System.Drawing.Size(132, 22);
             this.TBPajak.TabIndex = 11;
             // 
             // TBDiskon
             // 
-            this.TBDiskon.Location = new System.Drawing.Point(391, 231);
+            this.TBDiskon.Location = new System.Drawing.Point(479, 316);
+            this.TBDiskon.Margin = new System.Windows.Forms.Padding(4);
             this.TBDiskon.Name = "TBDiskon";
-            this.TBDiskon.Size = new System.Drawing.Size(100, 20);
+            this.TBDiskon.Size = new System.Drawing.Size(132, 22);
             this.TBDiskon.TabIndex = 12;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(267, 210);
+            this.label5.Location = new System.Drawing.Point(314, 290);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.Size = new System.Drawing.Size(63, 16);
             this.label5.TabIndex = 13;
             this.label5.Text = "Pajak % :";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(266, 237);
+            this.label6.Location = new System.Drawing.Point(313, 324);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.Size = new System.Drawing.Size(70, 16);
             this.label6.TabIndex = 14;
             this.label6.Text = "Diskon % :";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(28, 104);
+            this.label7.Location = new System.Drawing.Point(37, 128);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.Size = new System.Drawing.Size(77, 16);
             this.label7.TabIndex = 15;
             this.label7.Text = "Uang Tunai";
             // 
+            // BtnHapus
+            // 
+            this.BtnHapus.Location = new System.Drawing.Point(184, 240);
+            this.BtnHapus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BtnHapus.Name = "BtnHapus";
+            this.BtnHapus.Size = new System.Drawing.Size(93, 26);
+            this.BtnHapus.TabIndex = 16;
+            this.BtnHapus.Text = "Hapus";
+            this.BtnHapus.UseVisualStyleBackColor = true;
+            this.BtnHapus.Click += new System.EventHandler(this.BtnHapus_Click);
+            // 
             // FormTransaksi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnHapus);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -222,7 +238,7 @@ namespace TubesKPL
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.BoxObat);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormTransaksi";
             this.Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)(this.TabelKeranjang)).EndInit();
@@ -249,5 +265,6 @@ namespace TubesKPL
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnHapus;
     }
 }
